a3f0b4f1a10e6ea15a0ad9c569200d70cdd01a33
mustafijul <mustafijulislam21@gmail.com>
2026-04-01 11:44:09 +0600

    shadcn components added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
 
 import "./globals.css";
+import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
 
- 
-
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
+      lang="en" className={cn("font-sans", inter.variable)}
     >
       <body className="min-h-full flex flex-col font-poppins antialiased">{children}</body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center">
       <h1 className="text-4xl font-bold mb-4">GadgetBD</h1>
       <p className="text-gray-400 bg-shop-light-pink">Your #1 Gadget Store in Bangladesh Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore ab totam alias omnis numquam libero quo quis praesentium nostrum sunt consectetur dolore optio deleniti rem, architecto tenetur suscipit distinctio similique.</p>
+      <Button size="lg" variant="destructive">Buy now</Button>
     </main>
   );
 }
