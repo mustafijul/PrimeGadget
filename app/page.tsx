117d38b83e2b1356f18653c6c93e2fe5f62c907e
mustafijul <mustafijulislam21@gmail.com>
2026-04-01 12:20:49 +0600

    logo updated and navbar section design started

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="min-h-screen flex flex-col items-center justify-center">
       <h1 className="text-4xl font-bold mb-4">GadgetBD</h1>
       <p className="text-gray-400 bg-shop-light-pink">Your #1 Gadget Store in Bangladesh Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore ab totam alias omnis numquam libero quo quis praesentium nostrum sunt consectetur dolore optio deleniti rem, architecto tenetur suscipit distinctio similique.</p>
-      <Button size="lg" variant="destructive">Buy now</Button>
+      <Button className="mt-2" size="lg" variant="destructive">Buy now</Button>
     </main>
   );
 }
